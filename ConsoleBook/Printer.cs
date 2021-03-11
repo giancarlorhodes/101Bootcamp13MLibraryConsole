9380a6aab6d304f1712e25efeda1b48d892cfeb4
Giancarlo Rhodes <giancarlo.rhodes@onshoreoutsourcing.com>
2021-03-10 19:12:37 -0600

    more code

---
+++ ConsoleBook/Printer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLibrary
+{
+    class Printer
+    {
+
+        public List<string> Tables { get; }
+
+        public Printer(List<string> ts)
+        {
+            Tables = ts;
+        }
+
+        internal void PrintTables()
+        {
+            Console.WriteLine("****************  TABLES ***********************");
+            foreach (var _t in Tables)
+            {
+                Console.WriteLine(_t);
+            }
+        }
+    }
+}
--- ConsoleBook/Program.cs
+++ ConsoleBook/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleBook
+namespace ConsoleLibrary
 {
     class Program
     {
@@ -12,6 +12,8 @@ namespace ConsoleBook
         {
             Console.WriteLine("********************* Library App Console Starting  ***********************");
             Console.WriteLine("MENU OPTIONS: P print, A add, D delete, U update, Q quit and Enter");
+            Console.WriteLine();
+
             var _option = Console.ReadLine().ToUpper();
 
             while (_option != "Q") {
@@ -21,27 +23,40 @@ namespace ConsoleBook
 
                     case "P": {
 
-                        // print implementation
-                        break;
+                            // print implementation
+                            // Console.WriteLine("print"); // TESTING ONLY
+                            List<string> _ts = new System.Collections.Generic.List<string>();
+                            _ts.Add(TableName.Author.ToString());
+                            _ts.Add(TableName.Book.ToString());
+                            Printer _printer = new Printer(_ts);
+                            _printer.PrintTables();
+                            break;
                     }
 
                     case "A": {
 
-                        // adding implementaton
-                        break;
+                            // adding implementaton for SQLSSERVER DATABASE ADO.NET
+                            Console.WriteLine("adding"); // TESTING ONLY
+                            break;
                     }
 
                     case "D": {
-                        // delete implmentation
-                        break;
+                            // delete implmentation  for SQLSSERVER DATABASE ADO.NET
+                            Console.WriteLine("delete");  // TESTING ONLY
+                            break;
                     }
 
                     case "U": {
-                        // update implemtation
-                        break;
+                            // update implemtation  for SQLSSERVER DATABASE ADO.NET
+                            Console.WriteLine("update");  // TESTING ONLY
+                            break;
                     }
                 
                 }
+                Console.WriteLine("MENU OPTIONS: P print, A add, D delete, U update, Q quit and Enter");
+                _option = Console.ReadLine().ToUpper();
+                Console.WriteLine();
+
             }
 
             Console.WriteLine("********************* Library App Console Ending  ***********************");
