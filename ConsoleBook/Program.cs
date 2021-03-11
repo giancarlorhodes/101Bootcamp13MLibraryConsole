380afbd77a1cc3cde2cc012dc02d28ad3e7bbff4
Giancarlo Rhodes <giancarlo.rhodes@onshoreoutsourcing.com>
2021-03-10 18:45:45 -0600

    change project and solution file names

--- ConsoleBook/Program.cs
+++ ConsoleBook/Program.cs
@@ -10,7 +10,42 @@ namespace ConsoleBook
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("********************* Library App Console Starting  ***********************");
+            Console.WriteLine("MENU OPTIONS: P print, A add, D delete, U update, Q quit and Enter");
+            var _option = Console.ReadLine().ToUpper();
 
+            while (_option != "Q") {
+
+                switch (_option) 
+                {
+
+                    case "P": {
+
+                        // print implementation
+                        break;
+                    }
+
+                    case "A": {
+
+                        // adding implementaton
+                        break;
+                    }
+
+                    case "D": {
+                        // delete implmentation
+                        break;
+                    }
+
+                    case "U": {
+                        // update implemtation
+                        break;
+                    }
+                
+                }
+            }
+
+            Console.WriteLine("********************* Library App Console Ending  ***********************");
+            Console.ReadLine();
         }
     }
 }
