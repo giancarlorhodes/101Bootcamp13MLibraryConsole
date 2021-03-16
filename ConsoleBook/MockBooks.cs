43beacbfab8d196bfab9f00e45500a83a3fcff18
Giancarlo Rhodes <giancarlo.rhodes@onshoreoutsourcing.com>
2021-03-16 16:02:44 -0500

    adding schema and data library script

--- ConsoleBook/MockBooks.cs
+++ ConsoleBook/MockBooks.cs
@@ -11,16 +11,14 @@ namespace ConsoleLibrary
 
         internal List<Book> Books { get; set; }
 
-
         public MockBooks() {
 
             Books = new List<Book>()
-            { new Book { Author_FK = 0, Description = "Description Book 1", Genre_FK = 0, IsPaperback = "Y", Price = 12.33M, PublishDate = DateTime.Now, Publisher_FK = 0, Title = "Book 1" },
-              new Book { Author_FK = 0, Description = "Description Book 2", Genre_FK = 0, IsPaperback = "Y", Price = 12.33M, PublishDate = DateTime.Now, Publisher_FK = 0, Title = "Book 2" },
-              new Book { Author_FK = 0, Description = "Description Book 3", Genre_FK = 0, IsPaperback = "N", Price = 12.33M, PublishDate = DateTime.Now, Publisher_FK = 0, Title = "Book 3" },
+            { 
+                new Book { Author_FK = 0, Description = "Description Book 1", Genre_FK = 0, IsPaperback = "Y", Price = 12.33M, PublishDate = DateTime.Now, Publisher_FK = 0, Title = "Book 1" },
+                new Book { Author_FK = 0, Description = "Description Book 2", Genre_FK = 0, IsPaperback = "Y", Price = 12.33M, PublishDate = DateTime.Now, Publisher_FK = 0, Title = "Book 2" },
+                new Book { Author_FK = 0, Description = "Description Book 3", Genre_FK = 0, IsPaperback = "N", Price = 12.33M, PublishDate = DateTime.Now, Publisher_FK = 0, Title = "Book 3" }
             };
-        
-        
         
         }
 
