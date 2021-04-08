e08dadd8cb4f9d7add2cf41aa53a54e4c8365efe
Giancarlo Rhodes <giancarlo.rhodes@onshoreoutsourcing.com>
2021-04-08 15:33:04 -0500

    09A MockGenresTable

--- ConsoleBook/DataEntity/User.cs
+++ ConsoleBook/DataEntity/User.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//namespace ConsoleLibrary
-//{
-//    //class User
-//    //{
-//    //    public int UserID { get; set; } // primary key property
-//    //    public string LastName { get; set; }
-//    //    public string FirstName { get; set; }
-//    //    public string UserName { get; set; }
-//    //    public string Password { get; set; }
-//    //    public int RoleID_FK {get;set;} // foreign key property
-//    //}
-//}
+namespace ConsoleLibrary
+{
+    class User
+    {
+        // data 
+        public int UserID { get; set; } // primary key property
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int RoleID_FK { get; set; } // foreign key property
+    }
+}
--- ConsoleBook/MockDataEntity/MockBooksTable.cs
+++ ConsoleBook/MockDataEntity/MockBooksTable.cs
@@ -1,26 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ConsoleLibrary.DataEntity;
 
 namespace ConsoleLibrary.MockDataEntity
 {
-    internal class MockBooks
+    internal class MockBooksTable
     {
-
+        // data
         internal List<Book> Books { get; set; }
 
-        public MockBooks() {
 
-            Books = new List<Book>()
+        // constructors
+        public MockBooksTable() {
+
+            this.Books = new List<Book>()
             { 
-                new Book { Author_FK = 0, Description = "Description Book 1", Genre_FK = 0, IsPaperback = "Y", Price = 12.33M, PublishDate = DateTime.Now, Publisher_FK = 0, Title = "Book 1" },
-                new Book { Author_FK = 0, Description = "Description Book 2", Genre_FK = 0, IsPaperback = "Y", Price = 12.33M, PublishDate = DateTime.Now, Publisher_FK = 0, Title = "Book 2" },
+                new Book { BookID = 1, Author_FK = 0, Description = "Description Book 1", Genre_FK = 0, IsPaperback = "Y", Price = 12.33M, PublishDate = DateTime.Now, Publisher_FK = 0, Title = "Book 1" },
+                new Book {  Author_FK = 0, Description = "Description Book 2", Genre_FK = 0, IsPaperback = "Y", Price = 12.33M, PublishDate = DateTime.Now, Publisher_FK = 0, Title = "Book 2" },
                 new Book { Author_FK = 0, Description = "Description Book 3", Genre_FK = 0, IsPaperback = "N", Price = 12.33M, PublishDate = DateTime.Now, Publisher_FK = 0, Title = "Book 3" }
             };
-        
+            Book newBook = new Book { Author_FK = 0, Description = "Description Book 4", Genre_FK = 0, IsPaperback = "Y", Price = 12.33M, PublishDate = DateTime.Now, Publisher_FK = 0, Title = "Book 4" };
+            newBook.Description = "Description Book 4+";
+            this.Books.Add(newBook);
         }
 
     }
